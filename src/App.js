75b964516dcee3cc84d131799767e1359c529aad
Dende <alegrria@gmail.com>
2018-12-12 13:53:21 +0100

    parsed books into shelves in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    readBooks: [],
+    currentlyReadingBooks: [],
+    wantToReadBooks: [],
+
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -13,8 +18,20 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+  componentDidMount(){
+      BooksAPI.getAll().then((allBooks) => {
+        this.setState({
+          readBooks: allBooks.filter((book) => book.shelf === 'read'),
+          currentlyReadingBooks: allBooks.filter((book) => book.shelf === 'currentlyReading'),
+          wantToReadBooks: allBooks.filter((book) => book.shelf === 'wantToRead')
+        })
+      })
+    }
+
   render() {
     return (
+      console.log(this.state),
+      console.log(this.state.readBooks),
       <div className="app">
         {this.state.showSearchPage ? (
           <div className="search-books">
@@ -49,22 +66,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
+                        <Book />
                       </li>
                       <li>
                         <div className="book">
