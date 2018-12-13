4b03c192329e0461a7645cfd931b3c11b5324666
Dende <alegrria@gmail.com>
2018-12-13 23:46:50 +0100

    made books a functional component

--- src/Books.js
+++ src/Books.js
@@ -2,32 +2,25 @@ import React, { Component } from 'react'
 import SelectShelf from './SelectShelf'
 import PropTypes from 'prop-types';
 
-class Books extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired,
-  };
-
-  render(){
+const Books = (props) => {
 
     return (
-      <ol className="books-grid"> {this.props.books.map((book) =>
+      <ol className="books-grid"> {props.books.map((book) =>
         <li key={book.id} >
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})`}}></div>
               <div className="book-shelf-changer">
-                <SelectShelf book={book} changeShelf={this.props.changeShelf} />
+                <SelectShelf book={book} changeShelf={props.changeShelf} />
               </div>
             </div>
             <div className="book-title">
               <p>{book.title}</p>
             </div>
-            <div className="book-authors">{book.authors.join(', ')}</div>
+            {book.authors && <div className="book-authors">{book.authors.join(', ')}</div>}
           </div>
         </li>
       )}</ol>
     )
-  }
 }
 export default Books
